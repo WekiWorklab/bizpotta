d14791325a395e66423a289ba04b2919fdcb2673
emmyokc1299 <emmyokc1299@gmail.com>
2022-08-11 16:41:21 +0100

    XSidebar SVG correction

--- components/XSidebar.js
+++ components/XSidebar.js
@@ -24,7 +24,7 @@ export default function XSidebar({show, setShow}) {
       <nav className="fixed md:hidden top-0 translate-x-0 w-[350px] pt-24 px-6 bg-gray-50 rounded-r-[20px] shadow-md flex-wrap items-center justify-between h-full z-[9999] ">
 
       <div className="absolute top-[40px] right-[20px]" onClick={() => setShow(false)}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-[20px] w-[20px]" fill="none" viewBox="absolute" stroke="#999999" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-[20px] w-[20px]" fill="none" viewBox="0 0 20 20" stroke="#999999" strokeWidth={2}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>
       </div>
