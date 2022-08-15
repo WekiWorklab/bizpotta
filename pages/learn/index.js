d877e525ee3f30757a70cc5382db55e2130e54ab
emmyokc1299 <emmyokc1299@gmail.com>
2022-08-15 17:38:15 +0100

    Corrected overwrite for index.js of learn page

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -1,28 +1,20 @@
-import React, { useState } from 'react'
-import Content from '../../components/Content'
+import React, {useState} from 'react'
 import Header from '../../components/Header'
 import Layout from '../../components/Layout-Components/Layout'
 import Sidebar from '../../components/Sidebar'
 import XSidebar from '../../components/XSidebar'
+import Content from '../../components/Content'
 
+const index = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
 
-const LearnersDashboard = () => {
-  const [show, setShow] = useState(false)
-
-  return ( 
-      <Layout>
-        <Content />
-      </Layout>
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+      
+      
   )
 }
 
-export default LearnersDashboard
-
-
-
-// <div className='relative'>
-      {/* <Header show = {show} setShow = {setShow}/>
-      <Content />
-      <Sidebar /> */}
-      {/* {show ? <XSidebar  show = {show} setShow = {setShow} /> : null} * Sidebar with toggle functionality */}
-    // </div>
+export default index
