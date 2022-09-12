c1b8d4c6a37c50713e4660c76713c6034f998253
David Adokuru <davidadokuru@gmail.com>
2022-09-12 14:25:45 +0100

    Update index.js

--- pages/creators/courses/index.js
+++ pages/creators/courses/index.js
@@ -1,18 +1,12 @@
-import React from 'react'
-import NewContent from '../../../components/CreatorsComponents/CourseLandingComponent/NewContent'
-import OldContent from '../../../components/CreatorsComponents/CourseLandingComponent/OldContent'
-import Layout from '../../../components/CreatorsComponents/LayoutComponents/Layout'
+import React from "react";
+import NewContent from "../../../components/CreatorsComponents/CourseLandingComponent/NewContent";
+import OldContent from "../../../components/CreatorsComponents/CourseLandingComponent/OldContent";
+import Layout from "../../../components/CreatorsComponents/LayoutComponents/Layout";
 
 const index = () => {
+  const newCreator = false;
 
-  const newCreator = false
+  return <Layout>{!newCreator ? <OldContent /> : <NewContent />}</Layout>;
+};
 
-  return (
-    <Layout>
-        {!newCreator ? <OldContent /> :
-        <NewContent />}
-    </Layout>
-  )
-}
-
-export default index
+export default index;
