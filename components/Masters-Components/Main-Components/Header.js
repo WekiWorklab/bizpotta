f9542168c5061fc2993c52f47f70d83c612a102a
emmyokc1299 <emmyokc1299@gmail.com>
2022-09-15 11:27:42 +0100

    Updates to header in masters page

--- components/Masters-Components/Main-Components/Header.js
+++ components/Masters-Components/Main-Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { card_image, logo } from '../../../public'
 
@@ -7,6 +7,18 @@ import {FiSearch} from 'react-icons/fi'
 import { IoNotificationsOutline } from 'react-icons/io5'
 
 const Header = () => {
+
+    const [showCat, setShowCat] = useState(false)
+    const [showSearch, setShowSearch] = useState(false)
+    
+    const handleCat = () => {
+        setShowCat(prev => !prev)
+    }
+
+    const handleSearch = () => {
+        setShowSearch(prev => !prev)
+    }
+
   return (
     <div className='w-full h-[130px] flex flex-row items-center justify-between px-10'>
         {/* Logo Section */}
@@ -17,10 +29,17 @@ const Header = () => {
 
         {/* Search Categories section */}
         <div className='flex items-center font-bold text-darkBlue'>
-            <p>All categories</p>
-            <AiOutlineDown className='ml-1 mr-4' size={14}/>
-            <p>Search</p>
-            <FiSearch className='ml-1' size={14}/>
+            <div className='flex items-center relative' >
+                <p>All categories</p>
+                <AiOutlineDown className='ml-1 mr-4 cursor-pointer' size={14} onClick={handleCat}/>
+                {showCat && <CategoryDropdown />}
+            </div>
+
+            <div className='flex items-center relative' >
+                <p>Search</p>
+                <FiSearch className='ml-1 cursor-pointer' size={14} onClick={handleSearch}/>
+                {showSearch && <SearchDropdown />}
+            </div>
         </div>
 
         {/* Accounts details */}
@@ -40,3 +59,44 @@ const Header = () => {
 }
 
 export default Header
+
+
+
+const CategoryDropdown = () => {
+
+    const categories = [
+        {tag: "arts", title: 'Arts & Entertainment'},
+        {tag: "usiness", title: 'Business'},
+        {tag: "community", title: 'Community & Government'},
+        {tag: "design", title: 'Design and Style'},
+        {tag: "food", title: 'Food'},
+        {tag: "home", title: 'Home & Lifestyle'},
+        {tag: "music", title: 'Music'},
+        {tag: "tech", title: 'Science and Technology'},
+        {tag: "sports", title: 'Sports and Gaming'},
+        {tag: "fitness", title: 'Wellness and Fitness'},
+        {tag: "writing", title: 'Writing'},
+    ]
+
+    return (
+        <div className='w-[250px] py-4 px-4 bg-white shadow-lg absolute top-8 left-0 z-10 font-normal rounded-md'>
+            {categories.map((el, index) => (
+                <p key = {index} className='w-full h-[40px] text-[15px] font-semibold flex items-center'>{el.title}</p>
+            ))}
+        </div>
+    )
+}
+
+
+const SearchDropdown = () => {
+
+    return(
+        <div className='absolute top-8 left-0 w-[250px] bg-gray-300 flex-col py-2 px-3 rounded-md z-20'>
+            <p className='text-[11px] text-gray-700 '>Search course, instructor, category</p>
+            <div className='flex flex-row items-center justify-between '>
+                <input className='outline-none w-[200px] bg-transparent h-[30px] text-[13px]' />
+                <FiSearch className='ml-1' size={10}/>
+            </div>
+        </div>
+    )
+}
