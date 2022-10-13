b4ac373317b196a553c086a555bf8b1789298309
emmyokc1299 <emmyokc1299@gmail.com>
2022-10-13 17:16:46 +0100

    changes

--- components/CreatorsComponents/CourseLandingComponent/CourseStructureComponent/FirstContent.js
+++ components/CreatorsComponents/CourseLandingComponent/CourseStructureComponent/FirstContent.js
@@ -3,7 +3,7 @@ import React from 'react'
 // import { TextEditor } from '../../TextEditor'
 
 
-import { AiFillCaretDown, AiOutlineLink } from 'react-icons/ai'
+import { AiFillCaretDown, AiOutlineCheck, AiOutlineLink } from 'react-icons/ai'
 import { BsPencil, BsThreeDotsVertical, BsTrash } from 'react-icons/bs'
 import {TiDocumentText} from 'react-icons/ti'
 import { IoCheckmarkCircleOutline } from 'react-icons/io5'
@@ -12,28 +12,12 @@ import NotesSuccessModal from './NotesSuccessModal'
 import { useState } from 'react'
 
 const FirstContent = () => {
+    const [state, setState] = useState({lecture: "", resources: "", projects: "", })
 
-    // const [initialState, setInitialState] = useState({section1: "", section2: '', section3: "", section4: ""})
-    // const [initialState, setInitialState] = useState({
-    //      one:[{lecture: "a"}, {resources: "b"}, {project: "c"}, {test: 'd'}, {assignment: "e"}],
-    //      two:[{lecture: ""}, {resources: ""}, {project: ""}, {test: ''}, {assignment: ""}]
-    // }
-    // )
-
-    const [initialState, setInitialState] = useState([
-        {lecture: 'a', resources: 'b', project: "c", test: "d", assignment: "e"},
-        {lecture: 'a', resources: 'b', project: "c", test: "d", assignment: "e"}
-    ]
-   )
-
-    const [success, setSuccess] = useState(false)
-    const [currentValue, setCurrentValue] = useState(null)
-    const [week, setWeek] = useState(null)
-    const [type, setType] = useState(null)
-
+    const arr = new Array(1).fill("")
+    
   return (
     <div className=''>
-{/* w-full border-[1px] rounded-sm border-gray-500 mt-8 pb-10 px-4 md:pl-10 */}
         <div className='w-full border-[1px] rounded-sm border-gray-500 mt-8 py-10 px-4 md:pl-10'>
                 <p className='text-[14px] font-bold'>Resources</p>
                 <p className='text-[13px] mt-4'>We have shared helpful of resources, if you need more assistance, kindly contact us on our Helpline or you can ask for help in <span className='text-seaBlue'>Bizpotta Instructor&#39;s Community</span></p>
@@ -58,7 +42,6 @@ const FirstContent = () => {
                 </div>
         </div>
 
-
             <div className='w-full border-[1px] rounded-sm border-gray-500 mt-8 py-10 md:pl-10 px-4'>
                 <div className='flex flex-row justify-between items-center'>
                     <div>Course Structure</div>
@@ -73,21 +56,21 @@ const FirstContent = () => {
                 </div>
 
                 <div className='mt-8 flex flex-col gap-y-8'>   
-                    <Section value = {initialState[0]} setSuccess = {setSuccess} setCurrentValue = {setCurrentValue} week = {1} setWeek = {setWeek} type = {type} setType = {setType} />
-                    
+                    {
+                        arr.map((el, index) => (
+                            <WeekSection key = {index} week_no={index + 1} />
+                        ))
+                    }
 
-                    <div className='w-[150px] h-[40px] flex justify-center items-center font-bold border rounded-md'> + Add Section</div>
+                    {/* <div className='w-[150px] h-[40px] flex justify-center items-center font-bold border rounded-md'> + Add Section</div> */}
                 </div>
 
-                <div className='flex justify-end'>
+                <div className='flex justify-end mt-8'>
                     <div className='w-[150px] h-[40px] flex justify-center items-center text-[13px] text-white font-bold border rounded-md bg-darkBlue'>
                         Save changes
                     </div>
                 </div>
             </div>
-
-
-            <NotesSuccessModal success = {success}  setSuccess = {setSuccess}  value = {currentValue} setCurrentValue = {setCurrentValue} week = {week} type = {type} initialState = {initialState} setInitialState = {setInitialState}/>
 
     </div>
   )
@@ -97,65 +80,72 @@ export default FirstContent
 
 
 
+const WeekSection = ({week_no}) => {
 
-
-const Section = ({value, setSuccess, setCurrentValue, week, setWeek, type, setType}) => {
-
-    const handleClick = (clickedType) => {
-        setCurrentValue(value[type])
-        
-        setWeek(week)
-        setType(clickedType)
-        // setTimeout(() => {
-            setSuccess(true)
-        // }, 1000);
-
-        // This setTimeout was used because the setstate is asyncronous and setCurrentValue takes time
-    }
-
-    return(
-        <div className='w-full'>
+    const [editWeekTitle, setEditWeekTitle] = useState(false) //controls the ability to change the title of the week
+    const [collapseWeek, setCollapseWeek] = useState(true)  //controls collapse and expansion of week
+    
+    return (
+        <div className='w-full py-2'>
             <div className='w-full'>
                 <div className='flex w-full justify-between items-center'>
-                <div>Week 1 <span className='text-gray-400 text-[14px]'>Introduction</span></div> 
-                <BsTrash color='red' size={14}/> 
+                    <div className='flex items-center gap-x-3'>
+                        <div>Week {week_no} </div>
+                        {!editWeekTitle && <div className='flex items-center gap-x-2 text-gray-400 text-[14px] '>
+                            <p>Introduction</p>
+                            <BsPencil size={12} className = 'cursor-pointer' onClick = {() => {setEditWeekTitle(true); }}/>
+                        </div>}
+                        {editWeekTitle && <div className='flex items-center text-[14px] gap-x-2'>
+                            <input type = 'text' placeholder='introduction' className='focus:ring-0 outline-none rounded-sm h-[24px]'  />
+                            <div className='text-[#FF0000] cursor-pointer' onClick={() => {setEditWeekTitle(false)}}>X</div>
+                            <AiOutlineCheck color='green' className = 'cursor-pointer' onClick={() => {setEditWeekTitle(false)}}/>
+                        </div>
+                        }
+                    </div>
+
+                    <div className=''>
+                        {collapseWeek && <div className='text-[#999999] font-bold cursor-pointer' onClick={() => setCollapseWeek(false)}>Expand section</div>}
+                        {!collapseWeek && <div className='text-[#999999] font-bold cursor-pointer' onClick={() => setCollapseWeek(true)}>Collpase section</div>}
+                    </div>
+
+                    <BsTrash color='red' size={14}/> 
                 </div>
  
-                {/* Lecture */}
-                <div>
-                <div className='flex items-center gap-x-2 mt-4'>
-                    <TiDocumentText />
-                    <p>Lecture</p>
-                    <BsPencil  size={12}/>
-                    <AiFillCaretDown />
-                </div>
+                {!collapseWeek && 
+                    <div>
+                    {/* Lecture */}
+                    <div>
+                        <div className='flex items-center gap-x-2 mt-4'>
+                            <TiDocumentText />
+                            <p>Lecture</p>
+                            <BsPencil  size={12}/>
+                            <AiFillCaretDown />
+                        </div>
 
-                <div className='mt-6'>
-                    {/* <TextEditor /> */}
-                    <div className=' min-h-[150px] w-full md:w-[480px] sm:w-full border  break-all p-2' onClick={() => handleClick('lecture')}>
-                        {value.lecture}
-                    </div>
-                </div>
+                        <div className='mt-6'>
+                            <div className=' min-h-[150px] w-full border break-all p-2' onClick={() => {/**Take the user to the lecture edit page */}}>
+                                
+                            </div>
+                        </div>
 
-                <div className='flex flex-col gap-y-2 mt-4'>
-                    <div className='flex flex-row items-center gap-x-3'>
-                    <div className=' font-bold border rounded-md w-full md:w-[350px] h-[50px] flex flex-row items-center gap-x-2 pl-2'>
-                            <IoCheckmarkCircleOutline className='' size={26} color='#94F236'/>
-                            <p>Video upload successful</p>
-                        </div> 
-                        <p className='text-red-300'>X</p>
-                    </div>
+                        <div className='flex flex-col gap-y-2 mt-4'>
+                            <div className='flex flex-row items-center gap-x-3'>
+                            <div className=' font-bold border rounded-md w-full md:w-[350px] h-[50px] flex flex-row items-center gap-x-2 pl-2'>
+                                    <IoCheckmarkCircleOutline className='' size={26} color='#94F236'/>
+                                    <p>Video upload successful</p>
+                                </div> 
+                                <p className='text-red-300'>X</p>
+                            </div>
 
-                    <div className='flex flex-row items-center gap-x-3'>
-                    <div className=' font-bold border rounded-md w-full md:w-[350px] h-[50px] flex flex-row items-center gap-x-2 pl-2'>
-                            <IoCheckmarkCircleOutline className='' size={26} color='#94F236'/>
-                            <p>Content upload successful</p>
-                        </div> 
-                        <p className='text-red-300'>X</p>
+                            <div className='flex flex-row items-center gap-x-3'>
+                            <div className=' font-bold border rounded-md w-full md:w-[350px] h-[50px] flex flex-row items-center gap-x-2 pl-2'>
+                                    <IoCheckmarkCircleOutline className='' size={26} color='#94F236'/>
+                                    <p>Content upload successful</p>
+                                </div> 
+                                <p className='text-red-300'>X</p>
+                            </div>
+                        </div>
                     </div>
-                    
-                </div>
-                </div>
 
 
                 {/* Resources */}
@@ -164,7 +154,6 @@ const Section = ({value, setSuccess, setCurrentValue, week, setWeek, type, setTy
                         <div className='flex items-center gap-x-2 mt-4'>
                             <TiDocumentText />
                             <p>Resources</p>
-                            {/* <BsPencil  size={12}/> */}
                             <AiFillCaretDown />
                         </div>
                         <BsTrash color='red' size={14}/> 
@@ -172,8 +161,7 @@ const Section = ({value, setSuccess, setCurrentValue, week, setWeek, type, setTy
 
                 <div className='mt-6'>
                     {/* <TextEditor /> */}
-                    <div className='min-h-[150px] w-full md:w-[480px] sm:w-full border break-all p-2' onClick={() => handleClick('resources')}>
-                    {value.resources}
+                    <div className=' min-h-[150px] w-full border break-all p-2' >
                     </div>
                 </div>
 
@@ -197,70 +185,23 @@ const Section = ({value, setSuccess, setCurrentValue, week, setWeek, type, setTy
                 </div>
                 </div>
 
-
-
-                {/* Projects */}
-                <div className='mt-20'>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center gap-x-2 mt-4'>
-                            <TiDocumentText />
-                            <p>Project</p>
-                            {/* <BsPencil  size={12}/> */}
-                            <AiFillCaretDown />
-                        </div>
-                        <BsTrash color='red' size={14}/> 
-                    </div>
-
-                    <div className='mt-6'>
-                        {/* <TextEditor /> */}
-                        <div className='min-h-[150px] w-full md:w-[480px] sm:w-full border break-all p-2' onClick={() => handleClick('project')}>
-                        {value.project}
-                        </div>
-                    </div>
-
-                    <div className='flex flex-col gap-y-2 mt-4'>
-                        <div className='flex flex-row items-center gap-x-3'>
-                        <div className=' font-bold border rounded-md w-full md:w-[480px] h-[50px] flex flex-row items-center gap-x-2 pl-2'>
-                                <AiOutlineLink className='' size={26} color='#999999'/>
-                                <p>Paste link to resources</p>
-                            </div> 
-                            
-                        </div>
-
-                        <div className='flex flex-row items-center gap-x-3'>
-                        <div className=' font-bold border rounded-md w-full md:w-[480px] h-[50px] flex flex-row items-center gap-x-2 pl-2'>
-                                <AiOutlineLink className='' size={26} color='#999999'/>
-                                <p>Paste link to resources</p>
-                            </div> 
-                            
-                        </div>
-                        
-                    </div>
-                </div>
-
-
                 {/* Quiz/Test/Exam */}
                 <div className='mt-20'>
                     <div className='flex items-center justify-between'>
                         <div className='flex items-center gap-x-2 mt-4'>
                             <TiDocumentText />
                             <p>Quiz/Test/Exam</p>
-                            {/* <BsPencil  size={12}/> */}
                             <AiFillCaretDown />
                         </div>
                         <BsTrash color='red' size={14}/> 
                     </div>
 
-                <div className='mt-6'>
-                    {/* <TextEditor /> */}
-                    <div className='min-h-[150px] w-full md:w-[480px] sm:w-full border break-all p-2' onClick={() => handleClick('test')}>
-                    {value.test}
+                    <div className='mt-6'>
+                        <div className=' min-h-[150px] w-full border break-all p-2' >
+
+                        </div>
                     </div>
                 </div>
-
-            
-                </div>
-
 
                 {/* Assignment */}
                 <div className='mt-20'>
@@ -268,28 +209,25 @@ const Section = ({value, setSuccess, setCurrentValue, week, setWeek, type, setTy
                         <div className='flex items-center gap-x-2 mt-4'>
                             <TiDocumentText />
                             <p>Assignment</p>
-                            {/* <BsPencil  size={12}/> */}
                             <AiFillCaretDown />
                         </div>
                         <BsTrash color='red' size={14}/> 
                     </div>
+                    <div className='mt-6'>
+                        <div className=' min-h-[150px] w-full border break-all p-2'>
 
-                <div className='mt-6'>
-                    {/* <TextEditor /> */}
-                    <div className='min-h-[150px] w-full md:w-[480px] sm:w-full border break-all p-2' onClick={() => handleClick('assignment')}>
-                    {value.assignment}
+                        </div>
                     </div>
-                </div>
-
                
                 </div>
-
-
+                </div>}
                 
             </div>
         </div>
     )
+
 }
 
 
 
+{/* <NotesSuccessModal success = {success}  setSuccess = {setSuccess}  value = {currentValue} setCurrentValue = {setCurrentValue} week = {week} type = {type} initialState = {initialState} setInitialState = {setInitialState}/> */}
--- components/CreatorsComponents/CourseLandingComponent/OldContent.js
+++ components/CreatorsComponents/CourseLandingComponent/OldContent.js
@@ -10,7 +10,7 @@ import VideoRequest from './VideoRequest'
 
 const OldContent = () => {
 
-    const [select, setSelect] = useState('landing')
+    const [select, setSelect] = useState('structure')
 
     const dispatch = useDispatch()
 
@@ -21,13 +21,13 @@ const OldContent = () => {
 
 
   return (
-    <div className='relative w-full h-full bg-white flex flex-col mt-[90px] md:mt-[120px] md:justify-center items-start md:translate-x-[250px] md:w-[calc(100%-250px)] px-2 xl:px-16 py-10 redBorder text-darkGray'>
+    <div className='relative w-full h-full bg-white flex flex-col mt-[90px] md:mt-[120px] md:justify-center items-start md:translate-x-[250px] md:w-[calc(100%-250px)] px-2 xl:px-16 py-10 text-darkGray'>
           <div className='flex flex-row gap-x-6 text-[13px] md:text-[15px] font-bold justify-center'>
-                <p className={`${(select === "instructor") ? "border-b-4" : ""} border-bizpotta-green px-2`} onClick = {() => setSelect('instructor')}>Instructor&#39;s Profile</p>
-                <p className={`${(select === "video") ? "border-b-4" : ""} border-bizpotta-green px-2`} onClick = {() => setSelect('video')}>Live video request</p>
-                <p className={` ${(select === "landing") ? "border-b-4" : ""} border-bizpotta-green px-2`} onClick = {() => setSelect('landing')}>Course Landing Page</p>
-                <p className={`${(select === "structure") ? "border-b-4" : ""} border-bizpotta-green px-2`} onClick = {() => setSelect('structure')}>Course Structure</p>
-                <p className={`${(select === "pricing") ? "border-b-4" : ""} border-bizpotta-green px-2`} onClick = {() => setSelect('pricing')}>Pricing</p>
+                <p className={`${(select === "instructor") ? "bg-[#94F236] rounded-md bg-opacity-[0.06]" : "bg-transparent"}  px-3 py-2`} onClick = {() => setSelect('instructor')}>Instructor&#39;s Profile</p>
+                <p className={`${(select === "video") ? "bg-[#94F236] rounded-md bg-opacity-[0.06]" : "bg-transparent"}  px-3 py-2`} onClick = {() => setSelect('video')}>Live video request</p>
+                <p className={` ${(select === "landing") ? "bg-[#94F236] rounded-md bg-opacity-[0.06]" : "bg-transparent"}  px-3 py-2`} onClick = {() => setSelect('landing')}>Course Landing Page</p>
+                <p className={`${(select === "structure") ? "bg-[#94F236] rounded-md bg-opacity-[0.06]" : "bg-transparent"}  px-3 py-2`} onClick = {() => setSelect('structure')}>Course Structure</p>
+                <p className={`${(select === "pricing") ? "bg-[#94F236] rounded-md bg-opacity-[0.06]" : "bg-transparent"}  px-3 py-2`} onClick = {() => setSelect('pricing')}>Pricing</p>
             </div>
 
             {( select === "instructor" ) && <InstructorProfile />}
