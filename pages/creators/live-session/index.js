a9bbfb1cbf76f41208fd97637857921047515ae7
emmyokc1299 <emmyokc1299@gmail.com>
2022-10-21 17:45:07 +0100

    changes

---
+++ pages/creators/live-session/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../../components/CreatorsComponents/LayoutComponents/Layout'
+import Content from '../../../components/CreatorsComponents/LiveSessionComponents/Content'
+
+const LiveSession = () => {
+  return (
+    <Layout>
+        <Content />
+    </Layout>
+
+  )
+}
+
+export default LiveSession
