11179cdd47f5445823d334ea585d19a15949c6de
David Adokuru <davidadokuru@gmail.com>
2022-10-31 10:00:16 +0100

    Update Content.js

--- components/ShoppingCart-Components/Content.js
+++ components/ShoppingCart-Components/Content.js
@@ -18,61 +18,55 @@ const Content = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-full bg-white  flex flex-col mt-[90px] md:mt-[120px]  md:translate-x-[200px] md:w-[calc(100%-200px)] px-2 py-10">
-      <div className="text-md font-bold">Shopping Cart</div>
+    <div className='relative w-full h-full bg-white  flex flex-col mt-[90px] md:mt-[120px]  md:translate-x-[200px] md:w-[calc(100%-200px)] px-2 py-10'>
+      <div className='text-md font-bold'>Shopping Cart</div>
 
-      <div className="flex flex-col lg:flex-row mt-8 justify-between xl:pr-10">
-        <div className="flex-col gap-y-4">
+      <div className='flex flex-col lg:flex-row mt-8 justify-between xl:pr-10'>
+        <div className='flex-col gap-y-4'>
           {cartItems?.map((el, index) => (
-            <CourseCard key={index} id={el} cartItems={cartItems} />
+            <CourseCard key={index} id={el} cartItems={cartItems} setCartItems={setCartItems} />
           ))}
         </div>
 
         <CouponCard />
       </div>
 
-      <div className="flex flex-col mt-10 xl:flex-row  w-full">
-        <div className="bg-white w-full m-auto  ">
-          <div className="pt-[50px] flex flex-col">
-            <p className="text-[14px] mb-4 md:text-md font-semibold md:mb-8">
-              Because you liked a course on fashion designing
-            </p>
-            <div className="block w-[360px] m-auto md:hidden w-full">
+      <div className='flex flex-col mt-10 xl:flex-row  w-full'>
+        <div className='bg-white w-full m-auto  '>
+          <div className='pt-[50px] flex flex-col'>
+            <p className='text-[14px] mb-4 md:text-md font-semibold md:mb-8'>Because you liked a course on fashion designing</p>
+            <div className='block w-[360px] m-auto md:hidden w-full'>
               <ContentCarousel data={Data} screen={[1.5, 10]} />
             </div>
-            <div className="hidden m-auto md:block lg:hidden w-full">
+            <div className='hidden m-auto md:block lg:hidden w-full'>
               <ContentCarousel data={Data} screen={[2, 20]} />
             </div>
-            <div className="hidden lg:block w-full">
+            <div className='hidden lg:block w-full'>
               <ContentCarousel data={Data} screen={[4, 30]} />
             </div>
           </div>
-          <div className="pt-[50px] flex flex-col">
-            <p className="text-[14px] mb-4 md:text-md font-semibold md:mb-8 ">
-              Based on recent activites
-            </p>
+          <div className='pt-[50px] flex flex-col'>
+            <p className='text-[14px] mb-4 md:text-md font-semibold md:mb-8 '>Based on recent activites</p>
 
-            <div className="block w-[360px] m-auto md:hidden w-full">
+            <div className='block w-[360px] m-auto md:hidden w-full'>
               <ContentCarousel data={Data} screen={[1.5, 10]} />
             </div>
-            <div className="hidden m-auto md:block lg:hidden w-full">
+            <div className='hidden m-auto md:block lg:hidden w-full'>
               <ContentCarousel data={Data} screen={[2, 20]} />
             </div>
-            <div className="hidden lg:block w-full">
+            <div className='hidden lg:block w-full'>
               <ContentCarousel data={Data} screen={[4, 30]} />
             </div>
           </div>
-          <div className="pt-[50px] flex flex-col">
-            <p className="text-[14px] mb-4 md:text-md font-semibold md:mb-8 ">
-              Instructors
-            </p>
-            <div className="block w-[360px] m-auto md:hidden w-full">
+          <div className='pt-[50px] flex flex-col'>
+            <p className='text-[14px] mb-4 md:text-md font-semibold md:mb-8 '>Instructors</p>
+            <div className='block w-[360px] m-auto md:hidden w-full'>
               <InstructorCarousel data={instructorData} screen={[1.5, 10]} />
             </div>
-            <div className="hidden m-auto md:block lg:hidden w-full">
+            <div className='hidden m-auto md:block lg:hidden w-full'>
               <InstructorCarousel data={instructorData} screen={[2, 20]} />
             </div>
-            <div className="hidden lg:block w-full">
+            <div className='hidden lg:block w-full'>
               <InstructorCarousel data={instructorData} screen={[4, 30]} />
             </div>
           </div>
@@ -84,7 +78,7 @@ const Content = () => {
 
 export default Content;
 
-const CourseCard = ({ id }) => {
+const CourseCard = ({ id, setCartItems }) => {
   const router = useRouter();
 
   const RemoveItem = (ID) => {
@@ -92,40 +86,33 @@ const CourseCard = ({ id }) => {
     const newCart = cart.filter((el) => el != ID);
     localStorage.setItem("cart", JSON.stringify([...newCart]));
 
-    setTimeout(() => {
-      router.reload(window.location.pathname);
-    }, 1000);
+    setCartItems(newCart);
   };
 
   return (
-    <div className="w-full sm:w-[500px] sm:h-[140px] bg-white dropdown-shadow flex flex-col items-center py-3 sm:py-0 sm:flex-row px-10 sm:px-4 mb-8">
-      <div className="h-full flex items-center justify-start">
-        <img
-          src={offer.src}
-          alt=""
-          className="min-w-[85px] min-h-[85px] max-w-[85px] max-h-[85px] rounded-full "
-        />
+    <div className='w-full sm:w-[500px] sm:h-[140px] bg-white dropdown-shadow flex flex-col items-center py-3 sm:py-0 sm:flex-row px-10 sm:px-4 mb-8'>
+      <div className='h-full flex items-center justify-start'>
+        <img src={offer.src} alt='' className='min-w-[85px] min-h-[85px] max-w-[85px] max-h-[85px] rounded-full ' />
       </div>
-      <div className="flex flex-col items-center sm:items-start justify-center sm:ml-3 w-[350px]">
-        <p className="text-[14px] font-bold">Introduction to Hydroponics</p>
-        <p className="text-[11px] text-center sm:text-left text-gray-400 ">
-          By Introduction to Hydroponics Introduction to Hydroponics
-          Introduction to Hydroponics
+      <div className='flex flex-col items-center sm:items-start justify-center sm:ml-3 w-[350px]'>
+        <p className='text-[14px] font-bold'>Introduction to Hydroponics</p>
+        <p className='text-[11px] text-center sm:text-left text-gray-400 '>
+          By Introduction to Hydroponics Introduction to Hydroponics Introduction to Hydroponics
         </p>
-        <p className="text-[12px] text-gray-400">Price: #5000</p>
-        <div className="text-[12px] text-gray-400 flex flex-row items-center">
-          <p className="mr-2"> Rating. 4.7</p>
-          <AiFillStar size={14} color="#FFC700" />
-          <AiFillStar size={14} color="#FFC700" />
-          <AiFillStar size={14} color="#FFC700" />
-          <AiFillStar size={14} color="#FFC700" />
-          <AiFillStar size={14} color="#FFC700" />
+        <p className='text-[12px] text-gray-400'>Price: #5000</p>
+        <div className='text-[12px] text-gray-400 flex flex-row items-center'>
+          <p className='mr-2'> Rating. 4.7</p>
+          <AiFillStar size={14} color='#FFC700' />
+          <AiFillStar size={14} color='#FFC700' />
+          <AiFillStar size={14} color='#FFC700' />
+          <AiFillStar size={14} color='#FFC700' />
+          <AiFillStar size={14} color='#FFC700' />
         </div>
       </div>
-      <div className="text-[13px]  flex flex-col justify-center">
+      <div className='text-[13px]  flex flex-col justify-center'>
         {/* <div>Remove from cart</div> */}
         <div
-          className=" border-[1px] border-red-300 text-center cursor-pointer p-2 rounded-md hover:text-white hover:border-0 hover:bg-red-400 mt-3 sm:mt-0"
+          className=' border-[1px] border-red-300 text-center cursor-pointer p-2 rounded-md hover:text-white hover:border-0 hover:bg-red-400 mt-3 sm:mt-0'
           onClick={() => RemoveItem(id)}
         >
           Remove from cart
@@ -137,27 +124,23 @@ const CourseCard = ({ id }) => {
 
 const CouponCard = () => {
   return (
-    <div className="w-[300px] h-[350px] border shadow-lg bg-white px-5 py-10">
-      <p className="text-[14px] font-semibold">Apply Coupon</p>
+    <div className='w-[300px] h-[350px] border shadow-lg bg-white px-5 py-10'>
+      <p className='text-[14px] font-semibold'>Apply Coupon</p>
 
-      <div className="w-full h-[35px] border rounded-md flex pl-2 mt-6">
-        <input className="w-[80%] h-full outline-none text-[13px] " />
-        <div className="w-[20%] bg-darkBlue centerFlex text-[12px] rounded-r-md text-white ">
-          Apply
-        </div>
+      <div className='w-full h-[35px] border rounded-md flex pl-2 mt-6'>
+        <input className='w-[80%] h-full outline-none text-[13px] ' />
+        <div className='w-[20%] bg-darkBlue centerFlex text-[12px] rounded-r-md text-white '>Apply</div>
       </div>
 
-      <div className="w-full flex justify-between items-center mt-16">
-        <div className="text-[14px] font-semibold">Total</div>
-        <div className="text-[11px]">10% discount</div>
+      <div className='w-full flex justify-between items-center mt-16'>
+        <div className='text-[14px] font-semibold'>Total</div>
+        <div className='text-[11px]'>10% discount</div>
       </div>
 
-      <div className="text-[12px] mt-4">#10000</div>
+      <div className='text-[12px] mt-4'>#10000</div>
 
-      <div className="centerFlex mt-6">
-        <div className="w-[120px] h-[35px] centerFlex text-[13px] rounded-md bg-bizpotta-green">
-          Continue
-        </div>
+      <div className='centerFlex mt-6'>
+        <div className='w-[120px] h-[35px] centerFlex text-[13px] rounded-md bg-bizpotta-green'>Continue</div>
       </div>
     </div>
   );
