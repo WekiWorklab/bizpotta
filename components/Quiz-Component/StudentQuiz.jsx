06e63b991dd68b609d37f68a7083b8cf007625be
emmyokc1299 <emmyokc1299@gmail.com>
2022-11-10 16:16:29 +0100

    Changes to studentQuiz.js

--- components/Quiz-Component/StudentQuiz.jsx
+++ components/Quiz-Component/StudentQuiz.jsx
@@ -27,11 +27,11 @@ const StudentQuiz = () => {
   // final store for question and answer
   const finalAns = []
   const {id, weekId} = router.query
-  console.log(id, weekId)
+  // console.log(id, weekId)
 
    const fetchForQuiz = async (Id) => {
     const res = await learnersService.getMyCourse(Id.queryKey[1])
-    console.log(res.data)
+    // console.log(res.data)
     return res.data
   }
 
@@ -47,16 +47,9 @@ const StudentQuiz = () => {
     }
   })
   // console.log(data?.course?.course_weeks)
-  console.log('courseData -->', courseData)
-  console.log( "courseQuiz -->", courseQuiz)
-  /**Note: In the course_weeks array, i used the (weekId - 1) as the index of the element in the array that i will take as the actual course data for that the week.
-   * A better way is to run the onSuccess fucntion after every successsful query and get the required course data and corresponding course quiz for that week and store them in state. This was done to reduce length object chaining.
-   */
-
-  /**
-   * Just in case react query causes rerenders and state changes switch back to useEffect for the api call.
-   * Also in certain cases i didnt use the id for some data because the one coming in from the api is random. Instead i used hard numbers or index of elements in an array
-   */
+  // console.log('courseData -->', courseData)
+  // console.log( "courseQuiz -->", courseQuiz)
+  
 
   return (
     <div className='relative w-full h-full bg-[#FDFDFD] flex flex-col pt-[90px] md:pt-[120px] md:justify-start items-center md:translate-x-[200px] md:w-[calc(100%-200px)] px-2 lg:px-10  pb-10'>
@@ -64,13 +57,15 @@ const StudentQuiz = () => {
       <div className='w-full  mt-10'>
         <div className='w-full text-darkText mb-10'> {data?.course?.name} / Week {data?.course?.course_weeks?.filter(el => el.id == weekId)?.[0].week_number} / {data?.course?.course_weeks?.filter(el => el.id == weekId)?.[0].title}</div>
 
-       {quizState ?
+       {
+      //  quizState ?
 
         <QuizForSubmission quizState = {quizState} setQuizState = {setQuizState} answers = {answers} setAnswers = {setAnswers} finalAns = {finalAns} courseQuiz = {courseQuiz} courseId = {id} weekId = {weekId} returned = {returned} setReturned = {setReturned}
-        /> :
+        /> 
+        // :
+
         
-        <QuizAnsReview courseQuiz = {courseQuiz} returned = {returned}/>
-        // null
+        // <QuizAnsReview courseQuiz = {courseQuiz} returned = {returned} courseId = {id} weekId = {weekId} />
         
         }
 
@@ -94,13 +89,19 @@ const QuizAnsReview = ( {courseQuiz, quizState, setQuizState, answers, setAnswer
   }
 
 
+  console.log(courseQuiz)
 
   return (
     <div className='w-full flex flex-col gap-y-16'>
     {/* Questions */}
-    {
+    {/* {
       courseQuiz?.map((el, index) => (
         <QuestionCard key={index} el={el} elemIndex = {index + 1} proxyIndex = {el.id} quizState={quizState} answers = {answers} setAnswers = {setAnswers} returnedData = {returnedData} returned = {returned} />
+      ))
+    } */}
+    {
+    courseQuiz?.map((el, index) => (
+        <QuestionCard key={index} el={el} elemIndex = {index + 1} proxyIndex = {el.id}  quizState={quizState} answers = {answers} setAnswers = {setAnswers} returnedData = {returnedData} returned = {returned} />
       ))
     }
 
@@ -117,32 +118,43 @@ const QuizAnsReview = ( {courseQuiz, quizState, setQuizState, answers, setAnswer
 
 const QuizForSubmission = ({quizState, setQuizState, answers, setAnswers, finalAns, courseQuiz, courseId, weekId, setReturned, returned}) => {
 
-  const handleButton = async () => {
-    console.log("answers -->", answers)
+  const handleButton = () => {
+    router.push(`/students/quiz/${router.query.id}`)
+  }
 
-    // Check if all the questions were answered
-    for (const [key, value] of Object.entries(answers)) {
-      if (value == "") {
-        toast.error(`Please answer all questions`)
-        return
-      }
-    }
+
+  const handleSubmit = async () => {
+    // console.log("answers -->", answers)
 
     for (const[key, value] of Object.entries(answers)) {
       let question_id = Number(key.split("question")[1])
       finalAns.push({question_id: question_id, answer_id: value})
     }
-    console.log(finalAns)
+
+    // Check if all the questions were answered
+    if(finalAns.length != courseQuiz.length){
+      toast.error(`Please answer all questions`)
+      finalAns = []
+      return
+    }
+
+
+
+    // console.log(finalAns)
 
 
     // Submit the answers before switching quiz state
     const results = await learnersService.submitAnswers(finalAns, courseId, weekId )
     console.log( "results -->", results)
-    setReturned(results.data)
+    setReturned(results.data.StudentCourseWeekQuizAnswer)
+
+    console.log("results.data.StudentCourseWeekQuizAnswer --->", results.data.StudentCourseWeekQuizAnswer)
 
 
-
-    // setQuizState(false)  //switch from QuizForSubmission to QuizAnsReview
+    setTimeout(() => {
+      
+      setQuizState(false)  //switch from QuizForSubmission to QuizAnsReview
+    }, 1000);
   } 
 
   return (
@@ -151,12 +163,12 @@ const QuizForSubmission = ({quizState, setQuizState, answers, setAnswers, finalA
     {/* Questions */}
     {
     courseQuiz?.map((el, index) => (
-        <QuestionCard key={index} el={el} elemIndex = {index + 1} proxyIndex = {el.id}  quizState={quizState} answers = {answers} setAnswers = {setAnswers}  />
+        <QuestionCard key={index} el={el} elemIndex = {index + 1} proxyIndex = {el.id}  quizState={quizState} answers = {answers} setAnswers = {setAnswers} returned = {returned}  />
       ))
     }
 
     <div className='w-full flex justify-end lg:px-20'>
-        <Button onClick={handleButton} size = "w-[150px]" name = "Save Changes"/>
+        {quizState ? <Button onClick={handleSubmit} size = "w-[150px]" name = "Save Changes"/> : <Button onClick={handleButton} size = "w-[150px]" name = "Continue"/>}
     </div>
 
     </div>
@@ -166,7 +178,9 @@ const QuizForSubmission = ({quizState, setQuizState, answers, setAnswers, finalA
 
 
 const QuestionCard = ({el, proxyIndex, quizState, answers, setAnswers, returnedData, elemIndex, returned}) => {
-  // console.log(proxyIndex)  
+  // console.log(el.answers)  
+  console.log(returned)
+
   const [option, setOption] = useState(null)
 
   const handleClick = (optionId) => {
@@ -177,6 +191,24 @@ const QuestionCard = ({el, proxyIndex, quizState, answers, setAnswers, returnedD
     else {
       // .....
     }
+  }
+
+  /**
+   * for (el of returned) {
+   *  if (id == proxyIndex) {
+   *    return el
+   *    }
+   *  }
+   * 
+    answer_option_id
+    correct_answer_id
+    is_correct
+   */
+  const checkOptions = () => {
+      console.log(returned)
+     for (el of returned) {
+      if (el.id == proxyIndex){ return el}
+    } 
   }
 
 
@@ -192,17 +224,19 @@ const QuestionCard = ({el, proxyIndex, quizState, answers, setAnswers, returnedD
         <div className='flex flex-col gap-y-3 mt-4'>
           <div className='flex items-center gap-x-3'>
             <div className='w-full sm:w-[400px] lg:w-[550px] text-[14px] lg:text-base h-[55px] bg-[#F3F3F3] flex items-center justify-between px-3 rounded-md' >
-              <p className='break-words'>{el?.answers?.[0]?.title}</p>
-              {quizState ?
-                <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers[0].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[0].id)} />
-                : 
-                (returnedData ? <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${returnedData.results[proxyIndex - 1].answer_id == 1  ? "bg-darkBlue" : "bg-white" } `}/> : null)
-              }
+              <p className='break-words'>{el?.answers?.[0]?.title}   {el?.answers?.[0]?.id}</p>
+              <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers?.[0].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[0].id)} />
+              
             </div>
             {/* This is for controlling the good mark or bad mark sign is shown for the option */}
             {
-              !quizState ? (
-                returnedData.results[proxyIndex - 1].correct_id == 1 ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
+              !quizState  ? (
+                // <p>test</p>
+                returned?.length > 0 &&
+                (<div>
+                  <p>test</p>
+                  {checkOptions()?.correct_answer_id == el?.answers?.[0].id ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>}
+                </div>)
                 ) : null
               }
           </div>
@@ -210,36 +244,14 @@ const QuestionCard = ({el, proxyIndex, quizState, answers, setAnswers, returnedD
 
           <div className='flex items-center gap-x-3'>
             <div className='w-full sm:w-[400px] lg:w-[550px] text-[14px] lg:text-base h-[55px] bg-[#F3F3F3] flex items-center justify-between px-3 rounded-md' >
-            <p className='break-words'>{el?.answers?.[1]?.title}</p>
-              {quizState ? 
-              <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers[1].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[1].id)} />
-                : 
-                (returnedData ? <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${returnedData.results[proxyIndex - 1].answer_id == 2  ? "bg-darkBlue" : "bg-white" } `}/> : null)
+            <p className='break-words'>{el?.answers?.[1].title}   {el?.answers?.[1]?.id}</p>
+              {<div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el?.answers?.[1].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[1].id)} />
               }
             </div>
             {/* This is for controlling the good mark or bad mark sign is shown for the option */}
             {
-              !quizState ? (
-                returnedData.results[proxyIndex - 1].correct_id == 2 ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
-                ) : null
-              }
-          </div>
-
-
-
-          <div className='flex items-center gap-x-3'>
-            <div className='w-full sm:w-[400px] lg:w-[550px] text-[14px] lg:text-base h-[55px] bg-[#F3F3F3] flex items-center justify-between px-3 rounded-md' >
-            <p className='break-words'>{el?.answers?.[2]?.title}</p>
-              {quizState ? 
-                <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers[2].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[2].id)} />
-                : 
-                (returnedData ? <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${returnedData.results[proxyIndex - 1].answer_id == 3  ? "bg-darkBlue" : "bg-white" } `}/> : null)
-              }
-            </div>
-            {/* This is for controlling the good mark or bad mark sign is shown for the option */}
-            {
-              !quizState ? (
-                returnedData.results[proxyIndex - 1].correct_id == 3 ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
+              !quizState && returned?.length > 0 ? (
+                checkOptions()?.check_answer_id == el.answers?.[1].id ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
                 ) : null
               }
           </div>
@@ -248,17 +260,32 @@ const QuestionCard = ({el, proxyIndex, quizState, answers, setAnswers, returnedD
 
           <div className='flex items-center gap-x-3'>
             <div className='w-full sm:w-[400px] lg:w-[550px] text-[14px] lg:text-base h-[55px] bg-[#F3F3F3] flex items-center justify-between px-3 rounded-md' >
-            <p className='break-words'>{el?.answers?.[3]?.title}</p>
-              {quizState ? 
-                <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers[3].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[3].id)} />
-              : 
-              (returnedData ? <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${returnedData.results[proxyIndex - 1].answer_id == 4  ? "bg-darkBlue" : "bg-white" } `}/> : null)
+            <p className='break-words'>{el?.answers?.[2]?.title}    {el?.answers?.[2]?.id}</p>
+              {
+              <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers?.[2].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[2].id)} />
               }
             </div>
             {/* This is for controlling the good mark or bad mark sign is shown for the option */}
             {
-              !quizState ? (
-                returnedData.results[proxyIndex - 1].correct_id == 4 ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
+              !quizState && returned?.length > 0 ? (
+                checkOptions()?.check_answer_id == el.answers?.[2].id ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
+                ) : null
+              }
+          </div>
+
+
+
+          <div className='flex items-center gap-x-3'>
+            <div className='w-full sm:w-[400px] lg:w-[550px] text-[14px] lg:text-base h-[55px] bg-[#F3F3F3] flex items-center justify-between px-3 rounded-md' >
+            <p className='break-words'>{el?.answers?.[3]?.title}   {el?.answers?.[3]?.id}</p>
+              {
+              <div className={`w-[20px] h-[20px] rounded-full cursor-pointer ${option == el.answers?.[3].id ? "bg-darkBlue" : "bg-white" } `} onClick={() => handleClick(el.answers[3].id)} />
+              }
+            </div>
+            {/* This is for controlling the good mark or bad mark sign is shown for the option */}
+            {
+              !quizState && returned?.length > 0 ? (
+                checkOptions()?.check_answer_id == el.answers?.[3].id ? <FcCheckmark /> : <p className='text-[14px] text-red-500 font-bold'>X</p>
                 ) : null
               }
           </div>
@@ -271,6 +298,14 @@ const QuestionCard = ({el, proxyIndex, quizState, answers, setAnswers, returnedD
 }
 
 
+/**Note: In the course_weeks array, i used the (weekId - 1) as the index of the element in the array that i will take as the actual course data for that the week.
+   * A better way is to run the onSuccess fucntion after every successsful query and get the required course data and corresponding course quiz for that week and store them in state. This was done to reduce length object chaining.
+   */
+
+  /**
+   * Just in case react query causes rerenders and state changes switch back to useEffect for the api call.
+   * Also in certain cases i didnt use the id for some data because the one coming in from the api is random. Instead i used hard numbers or index of elements in an array
+   */
 
 
 const returnedData = {
