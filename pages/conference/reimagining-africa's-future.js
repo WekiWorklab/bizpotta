6fe4c74fff0d13097ed10ae840be7f7dfd0044d0
David Adokuru <davidadokuru@gmail.com>
2022-12-06 11:36:14 +0100

    updates

--- pages/conference/reimagining-africa's-future.js
+++ pages/conference/reimagining-africa's-future.js
@@ -1,15 +1,89 @@
-/* eslint-disable @next/next/no-img-element */
-// rafce
+import React from "react";
 
-import { useState } from "react";
-import { toast } from "react-toastify";
-import { Button } from "../components/Auth-Components/Button";
-import learnersService from "../services/LearnersService";
+import "./App.css";
+import { ZoomMtg } from "@zoomus/websdk";
 
-const Reimagine = () => {
-  const [data, setData] = useState({ name: "", email: "", city: "" });
+ZoomMtg.setZoomJSLib("https://source.zoom.us/2.9.5/lib", "/av");
 
-  return <h1>Register</h1>;
-};
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load("en-US");
+ZoomMtg.i18n.reload("en-US");
 
-export default Reimagine;
+function ReimagineConference() {
+  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
+  var signatureEndpoint = "";
+  // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
+  var sdkKey = process.env.NEXT_PUBLIC_ZOOM_SDK_KEY;
+  var meetingNumber = "123456789";
+  var role = 0;
+  var leaveUrl = "https://bizpotta.com";
+  var userName = "David";
+  var userEmail = "";
+  var passWord = "";
+  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
+  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
+  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
+  var registrantToken = "";
+
+  function getSignature(e) {
+    e.preventDefault();
+
+    fetch(signatureEndpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        meetingNumber: meetingNumber,
+        role: role,
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        startMeeting(response.signature);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function startMeeting(signature) {
+    document.getElementById("zmmtg-root").style.display = "block";
+
+    ZoomMtg.init({
+      leaveUrl: leaveUrl,
+      success: (success) => {
+        console.log(success);
+
+        ZoomMtg.join({
+          signature: signature,
+          meetingNumber: meetingNumber,
+          userName: userName,
+          sdkKey: sdkKey,
+          userEmail: userEmail,
+          passWord: passWord,
+          tk: registrantToken,
+          success: (success) => {
+            console.log(success);
+          },
+          error: (error) => {
+            console.log(error);
+          },
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  return (
+    <main>
+      <h1>Reimagining Africa&apos;s Future</h1>
+
+      <button onClick={getSignature}>Join Meeting</button>
+    </main>
+  );
+}
+
+export default ReimagineConference;
