daa493b5ced36c746d9f4a81d0d760a8f8b43de9
David Adokuru <45779326+adokuru@users.noreply.github.com>
2024-02-13 13:45:53 +0100

    updates

--- components/GreenFooter.js
+++ components/GreenFooter.js
@@ -55,7 +55,8 @@ const GreenFooter = () => {
         <div className='flex flex-row items-center text-white gap-x-4'>
           <p className='font-bold'>Copyright 2024</p>
           <p className='text-[13px] font-bold hidden md:block'>
-            Bridging the skill gap in Nigeria.
+            Bridging the skill gap in Nigeria. Enpowering the businesses with
+            tools for profitablity.
           </p>
         </div>
 
